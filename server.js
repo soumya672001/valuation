6836324b14197f4c8a883190d1e048314626929c
Soumyajit <soumya_672001@yahoo.com>
2016-08-20 18:14:12 +0530

    find query

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ var SampleApp = function() {
             	// log each of the first ten docs in the collection
             	var retJson;
            // 	db.policies.find({first_name: req.query.first_name}, {last_name: req.query.last_name}).forEach(function(err, doc) {
-            	policies.find({first_name: req.query.first_name}, {last_name: req.query.last_name}).forEach(function(err, doc) {  
+            	policies.find({first_name: req.query.first_name, last_name: req.query.last_name}).forEach(function(err, doc) {  
             	  if (err) throw err;
             	  if (doc) { console.dir(doc);
             	             retJson = doc.policies  }
